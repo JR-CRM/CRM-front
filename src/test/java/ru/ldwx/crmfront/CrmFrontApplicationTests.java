a32f906bc6041f22aaa9da2a81fd132a9bb33783
AlexeyK <ldwx@mail.ru>
2021-03-15 18:41:26 +0500

    init

---
+++ src/test/java/ru/ldwx/crmfront/CrmFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ldwx.crmfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
